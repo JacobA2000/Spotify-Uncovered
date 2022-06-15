f07c95cf5939a93f007ccfda28f1606e890479ec
Jacob Allen <jacoballen785@gmail.com>
2022-06-15 12:46:45 +0100

    Updates

---
+++ precache-manifest.8efbb368196c34c0055109469077f1ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5fe6162d1c2a05973fa",
+    "url": "/Spotify-Uncovered/static/css/main.d8b50236.chunk.css"
+  },
+  {
+    "revision": "a5fe6162d1c2a05973fa",
+    "url": "/Spotify-Uncovered/static/js/main.a5fe6162.chunk.js"
+  },
+  {
+    "revision": "bccd381e688c8fcf55dc",
+    "url": "/Spotify-Uncovered/static/css/1.c5132e0d.chunk.css"
+  },
+  {
+    "revision": "bccd381e688c8fcf55dc",
+    "url": "/Spotify-Uncovered/static/js/1.bccd381e.chunk.js"
+  },
+  {
+    "revision": "3133e6ed3d57c08f7a64",
+    "url": "/Spotify-Uncovered/static/js/runtime~main.3133e6ed.js"
+  },
+  {
+    "revision": "b694efa359ed90909b087a5129ba93f5",
+    "url": "/Spotify-Uncovered/index.html"
+  }
+];
